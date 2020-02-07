d0242d67b3b28772e8d576585eb604cbdd96038c
Bronyrus <bronyruschannel@gmail.com>
2020-02-07 11:18:01 +0300

    Update MasterWebController.php

--- app/Http/Controllers/Web/MasterWebController.php
+++ app/Http/Controllers/Web/MasterWebController.php
@@ -12,6 +12,7 @@ use App\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use libphonenumber;
