16495cbf0b0d52024344bedc57f673d473a143f3
Bronyrus <36816185+Bronyrus@users.noreply.github.com>
2020-04-19 21:42:46 +0300

    Update InquiryApiController.php

--- app/Http/Controllers/Api/InquiryApiController.php
+++ app/Http/Controllers/Api/InquiryApiController.php
@@ -96,7 +96,7 @@ class InquiryApiController extends ApiBaseController
             $test = new PushController();
             $request->request->add(['uuid' => $this->inquiry->uuid]);
             $masters = self::getMasterList($request);
-            foreach($masters->getData() as $master)
+            foreach($masters->getContent() as $master)
             {
                 return $this->sendResponse([$master],
             'Запрос успешно создан');
