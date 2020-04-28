f5ee12d58ade4f10bda3402746565bfda1773830
Bronyrus <36816185+Bronyrus@users.noreply.github.com>
2020-04-28 12:38:38 +0300

    Update InquiryApiController.php

--- app/Http/Controllers/Api/InquiryApiController.php
+++ app/Http/Controllers/Api/InquiryApiController.php
@@ -62,7 +62,7 @@ class InquiryApiController extends ApiBaseController
 
         dd($request);
 
-        // try {
+        try {
             DB::transaction(function () use ($request, $authClientId) {
                 $inquiry = Inquiry::create([
                     'uuid' => Str::uuid(),
@@ -92,9 +92,9 @@ class InquiryApiController extends ApiBaseController
                     }
                 }
             });
-        // } catch (\Throwable $th) {
-        //     return response()->json(['error'=>'Произошла ошибка'], 500);  
-        // }
+        } catch (\Throwable $th) {
+            return response()->json(['error'=>'Произошла ошибка'], 500);  
+        }
 
         if($request->urgency == 'urgent')
         {
